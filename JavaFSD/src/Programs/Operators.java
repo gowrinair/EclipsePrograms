13ee0ec0212f8356c1355984668f02815fb0bf53
gowri <gowri.nair097@gmail.com>
2021-08-06 16:36:05 +0530

    all java eclipse programs

---
+++ JavaFSD/src/Programs/Operators.java
@@ -0,0 +1,34 @@
+package Programs;
+
+public class Operators {
+	
+	public static void main(String[] args) {
+		
+		int a=15;
+		int b=2;
+		boolean c=false;
+		
+		System.out.println(a%b);
+		System.out.println(a/b);
+		
+		
+		System.out.println("a is:"+ a);
+		System.out.println(~a);
+		System.out.println(a++);
+		System.out.println(++a);
+		
+		System.out.println("b is: "+b);
+		System.out.println(~b);
+		System.out.println(b++);
+		System.out.println(++b);
+		
+		System.out.println(!c);
+		
+		
+		System.out.println(b<<a);
+		
+		
+		
+	}
+
+}
