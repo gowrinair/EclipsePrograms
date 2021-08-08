562c5cf98204a39d6c6dc3af00616c59ad1b9772
gowri <gowri.nair097@gmail.com>
2021-08-08 19:11:32 +0530

    commiting all JDBC programs

--- JDBC_API/src/Banking/JDBC_con.java
+++ JDBC_API/src/Banking/JDBC_con.java
@@ -2,25 +2,22 @@ package Banking;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class JDBC_con {
 	
 	public static void main(String[] args) {
 		try {
-			//Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/banking","root","root");
-			Statement stmnt=con.createStatement();
-			ResultSet rs=stmnt.executeQuery("select * from customer_details");
-			System.out.println("Customer Details:");
-			while(rs.next())
-				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getInt(3));
-			rs=stmnt.executeQuery("select * from account_details");
-			System.out.println("\nAccount Details: ");
-				while(rs.next())
-					System.out.println(rs.getInt(1)+"  "+rs.getInt(2)+"  "+rs.getString(3));
 			
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost","root","root");
+			Connection con1=DriverManager.getConnection("jdbc:mysql://localhost:3306/banking","root","root");
+			Statement stmnt=con.createStatement();
+			Statement st=con1.createStatement();
+			customer_details obj=new customer_details();
+			//obj.createDB(con, stmnt);
+			//obj.createCustomer_Details(con1, st);
+			obj.insertCustomer_Details(con1, st);
+			obj.selectCustomer_Details(con1, st);
 			con.close();
 		}
 		catch (Exception e) {
